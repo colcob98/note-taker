20251f6e85a685163b65ccb9c31e17584667ec56
Collier Cobb <collierj.cobb@gmail.com>
2023-10-09 14:48:56 -0400

    successfully initialized server.js with correct errors

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+// app.use('/', htmlRoutes);
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
