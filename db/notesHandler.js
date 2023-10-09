89ba3090863cd7694166763b9272f80744417c36
Collier Cobb <collierj.cobb@gmail.com>
2023-10-09 16:36:32 -0400

    troubleshooting cntd

--- db/notesHandler.js
+++ db/notesHandler.js
@@ -1,38 +1,54 @@
-const util = require('util');
-const fs = require('fs');
-const generateId = require('../helpers/uuid');
+const util = require("util");
+const fs = require("fs");
+const generateId = require("../helpers/uuid");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 class Notes {
-    read() {
-        return readFileAsync('db/db.json', 'utf8');
-    }
+  async read() {
+    return readFileAsync("db/db.json", "utf8");
+  }
 
-    write(note) {
-        return writeFileAsync('db/db.json', JSON.stringify(note));
-    }
+  write(note) {
+    return writeFileAsync("db/db.json", JSON.stringify(note));
+  }
 
-    getNotes(){
-        return this.read().then((notes) => {
-            return JSON.parse(notes) || [];
-        });
-    }
+  async getNotes() {
+    return this.read().then((notes) => {
+        let parsedNotes;
+        try {
+            parsedNotes = [].concat(JSON.parse(notes));
+        } catch (err) {
+            parsedNotes = [];
+        }
+        return parsedNotes;
+    })
+  }
 
-    saveNote(note){
-        note.id = generateId();
-        return this.getNotes().then(notes => {
-            notes.push(note);
-            return notes;
-        })
-        .then(notes => {
-            return this.write(JSON.stringify(notes));
-        })
-        .then(() => {
-            return note;
-        });
-    }
+  saveNote(note) {
+    note.id = generateId();
+    return this.getNotes()
+      .then((notes) => {
+        notes.push(note);
+        return notes;
+      })
+      .then((notes) => {
+        return this.write(JSON.stringify(notes));
+      })
+      .then(() => {
+        return note;
+      });
+  }
+  eraseNote(id) {
+    return this.getNotes()
+      .then((notes) => {
+        return notes.filter((note) => note.id !== id);
+      })
+      .then((notes) => {
+        return this.write(JSON.stringify(notes));
+      });
+  }
 }
 
 module.exports = Notes;
